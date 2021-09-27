6a11f1f6e0fb1560bcdf4d51cfacd85a97730d38
A.Mathew <BlueOrchid543@gmail.com>
2021-09-27 17:09:08 -0400

    eliminated warnings

--- src/test/java/ex26/App.java
+++ src/test/java/ex26/App.java
@@ -5,8 +5,31 @@
 package ex26;
 
 import java.util.Scanner;
+import java.lang.Math;
+
 
 public class App {
-    Scanner input = new Scanner(System.in);
+
+    public static int payOff(double b, double i, double p){
+        i /= 36500;
+        return (int) Math.ceil(-(1.0/30.0) * Math.log(1 + b/p * (1 - Math.pow(1 + i, 30))) / Math.log(1 + i));
+
+    }
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your balance? ");
+        double balance = input.nextDouble();
+        System.out.print("What is the APR on the card (as a percent)? ");
+        double APR = input.nextDouble();
+        System.out.print("What is the monthly payment you can make? ");
+        double monthPay = input.nextDouble();
+
+        int months = payOff(balance, APR, monthPay);
+
+        System.out.print("It will take you "+ months +" months to pay off this card.");
+
+    }
 
 }
