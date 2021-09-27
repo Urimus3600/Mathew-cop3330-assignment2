7eb671e12f7c31fe1b2c09c56b6e63683d3d3631
A.Mathew <BlueOrchid543@gmail.com>
2021-09-27 15:38:56 -0400

    ex25

---
+++ src/test/java/ex24/App.java
@@ -0,0 +1,10 @@
+package ex24;
+
+public class App {
+    public static void main(String[] args){
+
+        System.out.println("Hello World...\n");
+        System.out.println("Whats up");
+
+    }
+}
